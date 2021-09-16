84b83d5320af9d77a576d13d6ee7079e4097d127
aprinapina11 <aprinasitumorang17@gmail.com>
2021-09-16 17:29:25 +0700

    Upload awal project

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -4,8 +4,8 @@ class Contoh1 extends CI_Controller
     public function index()
     {
         echo "<h1>Perkenalkan</h1>";
-        echo "Nama saya Imam Nawawi 
-               saya tinggal di daerah Ciputat
+        echo "Nama saya Aprina Situmorang 
+               saya tinggal di daerah Pontianak Timur
                olah raga yang saya sukai adalah 
                Bulutangkis";
     }
